1f9ef703b2c882cffcf16a3e1cd6ecb5d49d6cc5
Pierre SCHAULI <pierrelixenbuhl@gmail.com>
2020-12-23 00:56:58 +0100

    Tags avec catégorie, tag avec choix et nouveaux tags
    == Description ==
    Les tags sont maintenant divisés en catégories.
    Ces catégories sont sélectionnables avec le type choice des slash commands.
    On peut aussi diviser un même tag en différents choix. Par exemple, quand on execute le [/tag category:programmation query:apprendre], il proposera soit python, soit javascript, et il est possible de très rapidement choisir.
    
    Les tags suivant ont été créer :
     - [programmation] apprendre
     - [programmation] database
     - [programmation] json
     - [general] ask

--- cogs/utils/misc.py
+++ cogs/utils/misc.py
@@ -1,22 +1,33 @@
 from schema import Schema, Or, And, Use, Optional
 
+embed_shema = Schema({
+    'embed': {
+        'title': str,
+        'description': Or(str, And(list, Use(lambda iterable: '\n'.join(iterable)))),
+        Optional('image'): {
+            'url': str
+        },
+        Optional('fields'): [
+            {
+                'name': str,
+                'value': Or(str, And(list, Use(lambda iterable: '\n'.join(iterable)))),
+                Optional('inline'): bool
+            }
+        ]
+    }
+})
+
 tag_shema = Schema({
     'name': str,
     'description': str,
-    'response': {
-        'embed': {
-            'title': str,
-            'description': Or(str, And(list, Use(lambda iterable: '\n'.join(iterable)))),
-            Optional('image'): {
-                'url': str
-            },
-            Optional('fields'): [
-                {
-                    'name': str,
-                    'value': Or(str, And(list, Use(lambda iterable: '\n'.join(iterable)))),
-                    Optional('inline'): bool
-                }
-            ]
+    'response': Or(embed_shema, {
+        'choice': {
+            str: embed_shema
         }
-    }
+    })
 })
+
+
+async def add_reactions(message, reactions) -> None:
+    for react in reactions:
+        await message.add_reaction(react)
