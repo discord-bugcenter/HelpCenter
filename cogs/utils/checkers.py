15d548aea3fe6ba9213b16b0db81aa199a69395a
AiroPi <47398145+AiroPi@users.noreply.github.com>
2020-12-23 23:42:30 +0100

    Nouveaux tags
    == Description ==
    Création du tag use_let, competition des tags apprendre et database.
    Le check de salon concerne maintenant les salons du serveur BugCenter (et non ceux de tests)

--- cogs/utils/checkers.py
+++ cogs/utils/checkers.py
@@ -4,9 +4,18 @@ from discord_slash import SlashContext
 
 
 def authorized_channels(func):
-    authorized_channels_id = [624312250806566932]
+    authorized_channels_id = [
+        692712497844584448,  # discussion-dev
+        595981741542604810,  # aide-dev
+        707555362458304663,  # aide-dev-2
+        779040873236136007,  # aide-dev-3
+        754322079418941441,  # aide-autres
+        780123502660681728,  # aide-autres-2
+        595224241742413844,  # tests-1
+        595224271132033024   # tests-2
+    ]
     formated_text = ("Vous ne pouvez pas executer cette commande ici. Essayez dans ces salons :\n"
-                     f"<#{'>, <#'.join(str(chann_id) for chann_id in authorized_channels_id)}>")
+                     f"<#{'>, <#'.join(str(chan_id) for chan_id in authorized_channels_id)}>")
 
     async def wrapper(*args, **kwargs):
         ctx = discord.utils.find(lambda arg: isinstance(arg, Context) or isinstance(arg, SlashContext), args)
