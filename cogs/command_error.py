5aeec1a1946dfaf5cd2def9fd3eeee8ae73dbbd6
AiroPi <47398145+AiroPi@users.noreply.github.com>
2021-01-09 20:33:21 +0100

    Gestion des erreurs finalisée
    == Description ==
    Le checker des channels concerne maintenant aussi la commande help.
    Les messages d'erreur prennent la même apparence que ceux de Bot Center.

--- cogs/command_error.py
+++ cogs/command_error.py
@@ -5,7 +5,7 @@ from discord.ext.commands import errors
 from .utils import custom_errors
 
 
-class CommandsError(commands.Cog):
+class CommandError(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -15,7 +15,8 @@ class CommandsError(commands.Cog):
             title="<:error:797539791545565184> Erreur",
             url="https://discord.gg/Drbgufc",
             description=error_message,
-            timestamp=ctx.message.created_at
+            timestamp=ctx.message.created_at,
+            color=discord.Color.from_rgb(0, 0, 0)
         )
         embed.set_author(
             name=f'{ctx.author.name}#{ctx.author.discriminator}',
@@ -26,7 +27,7 @@ class CommandsError(commands.Cog):
             icon_url=ctx.bot.user.avatar_url
         )
 
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embed, delete_after=10)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
@@ -34,6 +35,10 @@ class CommandsError(commands.Cog):
             pass
 
         elif isinstance(error, custom_errors.NotAuthorizedChannels):
-            formated_text = (f"Vous ne pouvez pas exécuter cette commande dans <#{error.channel.id}>. Essayez dans l'un de ces salons :\n"
+            formated_text = (f"Vous ne pouvez pas exécuter cette commande dans <#{error.channel.id}>. Essayez dans l'un de ces salons :\n\n"
                              f"<#{'>, <#'.join(str(chan_id) for chan_id in ctx.bot.authorized_channels_id)}>")
-            return self.send_error(ctx, formated_text)
+            return await self.send_error(ctx, formated_text)
+
+
+def setup(bot):
+    bot.add_cog(CommandError(bot))
