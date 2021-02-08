d890f20863c53529407e6e8981b72cdd40f68c85
AiroPi <47398145+AiroPi@users.noreply.github.com>
2021-02-08 21:31:25 +0100

    change: more proper code
    Command name changed & code cleaned

--- cogs/google_it.py
+++ cogs/google_it.py
@@ -1,28 +1,31 @@
-import discord
+from urllib import parse
+
 from discord.ext import commands
 
 from .utils import checkers
 
-from urllib import parse
 
 class LetMe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(
-        name='letme',
-        usage='/letme {query}',
+        name='googleit',
+        usage='/googleit {query}',
+        aliases=['lmgt', 'letme'],
         description='Montrer comment faire une recherche google :D'
     )
     @checkers.authorized_channels()
-    async def letme(self, ctx, *, array):
+    async def google_it(self, ctx, *, array):
         query = parse.quote_plus(array)
 
         if not query:
             raise commands.CommandError('Vous devez mettre une rechercher dans votre message ! *Regardez `/help`*')
 
-        await ctx.send(f'Comment rechercher **{array}** sur google\n**SUIS LE LIEN SUIVANT** --> https://letmegooglethat.com/?q={query}')
+        await ctx.send("L'outil google est fort puissant, regarde comment ça marche !\n"
+                       f"https://letmegooglethat.com/?q={query}")
         await ctx.message.delete()
+
 
 def setup(bot):
     bot.add_cog(LetMe(bot))
