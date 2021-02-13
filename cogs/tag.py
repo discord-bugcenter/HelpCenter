88ab4efe605426a061647f51f75d6c5e74762960
AiroPi <47398145+AiroPi@users.noreply.github.com>
2021-02-13 21:39:57 +0100

    Make tags bilingual
    [removed] tag author
    [added] support for bilingual tags
    [added] the command tag is now bilingual
    [changed] use of the "walrus" synthax for some things

--- cogs/tag.py
+++ cogs/tag.py
@@ -9,6 +9,7 @@ from schema import SchemaError
 
 from .utils.misc import tag_shema
 from .utils import checkers, misc
+from .utils.i18n import use_current_gettext as _
 
 
 class Tag(commands.Cog):
@@ -17,8 +18,8 @@ class Tag(commands.Cog):
 
         tags_folder = {
             category: {
-                    path.splitext(tag_name)[0]: path.join(path.join('ressources/tags/', category), tag_name) for tag_name in os.listdir(path.join('ressources/tags', category)) if path.isdir(path.join('ressources/tags', category))
-                } for category in os.listdir('ressources/tags/') if os.path.isdir(os.path.join('ressources/tags/', category))
+                    path.splitext(tag_name)[0]: path.join(category_path, tag_name) for tag_name in os.listdir(category_path)
+                } for category in os.listdir('ressources/tags/') if path.isdir(category_path := path.join('ressources/tags/', category))
         }
 
         def complete_values(obj, ref=None):
@@ -49,7 +50,7 @@ class Tag(commands.Cog):
                             self.bot.logger.warning(f'The tag {tag_name} from category {category_name} is improper.\n{e}')
                             continue
 
-                        self.tags[category_name][loaded_tag["name"]] = complete_values(loaded_tag)
+                        self.tags[category_name][(loaded_tag[0] if isinstance(loaded_tag, list) else loaded_tag)["name"]] = complete_values(loaded_tag)
 
                 except Exception as e:
                     print(e)
@@ -62,20 +63,20 @@ class Tag(commands.Cog):
     )
     @checkers.authorized_channels()
     async def _tag(self, ctx, category=None, *, query=None):
-        category_tags = self.tags.get(category)  # category_tags correspond a un dictionnaire avec plusieurs commandes
+        category_tags = self.tags.get(category)  # category_tags is a dict with categories of the tag
 
         if category_tags is None and category is not None:
             similors = ((name, SequenceMatcher(None, name, category).ratio()) for name in self.tags.keys())
             similors = sorted(similors, key=lambda couple: couple[1], reverse=True)
 
             if similors[0][1] > 0.8:
-                category = similors[0][0]  # nom de la catégorie
+                category = similors[0][0]  # category name
                 category_tags = self.tags.get(category)
 
-        if category_tags is None:
+        if category_tags is None:  # if the given category isn't ~= or == to any category
             format_list = lambda keys: "\n".join([f"- `{key}`" for key in keys])
             embed = discord.Embed(
-                title="Catégorie non trouvée. Essayez parmi :",
+                title=_("Category not found. Try among :"),
                 description=format_list(self.tags.keys()),
                 color=discord.Color.from_rgb(47, 49, 54)
             )
@@ -83,37 +84,46 @@ class Tag(commands.Cog):
             message = await ctx.send(embed=embed)
             return await misc.delete_with_emote(ctx, message)
 
-        if query is None or query == "list":
-            format_list = lambda tags_values: "\n".join([f"- `{tag.get('name')}` : {tag.get('description')}" for tag in tags_values])
-            message = await ctx.channel.send(embed=discord.Embed(title=f"Voici les tags de la catégorie `{category}` :",
+        def get_tag_lang(tag):
+            if isinstance(tag, dict):  # there is just one lang
+                return tag
+            found_tag = discord.utils.find(lambda in_tag: in_tag.get('lang') == self.bot.get_member_lang(ctx.author), tag)
+            return found_tag or tag[0]
+
+        if query is None or query == "list":  # if no tag name was given, or the tag name is "list"
+            format_list = lambda tags_values: "\n".join([f"- `{get_tag_lang(tag).get('name')}` : {get_tag_lang(tag).get('description')}" for tag in tags_values])
+            message = await ctx.channel.send(embed=discord.Embed(title=_("Here are the tags from the `{0}` category :").format(category),
                                                                  description=format_list(category_tags.values()),
                                                                  color=discord.Color.from_rgb(47, 49, 54))
                                              )
             return await misc.delete_with_emote(ctx, message)
 
-        tag = category_tags.get(query) or discord.utils.find(lambda tag_: tag_.get('aliases') and query in tag_['aliases'], category_tags.values())
+        selected_tag = category_tags.get(query) or discord.utils.find(lambda tag: (aliases := get_tag_lang(tag).get('aliases')) and query in aliases, category_tags.values())
 
-        if tag is None:
+        if selected_tag is None:  # given tag name not in tags or aliases
             similors = ((name, SequenceMatcher(None, name, query).ratio()) for name in category_tags.keys())
             similors = sorted(similors, key=lambda couple: couple[1], reverse=True)
 
             if similors[0][1] > 0.8:
-                query = similors[0][0]  # nom du tag
-                tag = category_tags.get(query)
+                query = similors[0][0]  # tag name
+                selected_tag = category_tags.get(query)
             else:
-                similar_text = f"voulez vous-vous dire `{similors[0][0]}` ? Sinon "
-                return await ctx.send(f"Le tag n'a pas été trouvé, {similar_text if similors[0][1] > 0.5 else ''}regardez `/tag list`", delete_after=10)
+                similar_text = _("do you mean `{0}` ? Otherwise ").format(similors[0][0])
+                return await ctx.send(_("Tag not found, {0}look `/tag list`").format(similar_text if similors[0][1] > 0.5 else ''), delete_after=10)
+
+        selected_tag = get_tag_lang(selected_tag)
 
         message = None
-        response = tag.get('response')
+        response = selected_tag.get('response')
         choices = response.get('choices')
         if choices:
             reactions = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣']
-            message = await ctx.send("__Choisissez la cible :__\n"+'\n'.join([f"{reactions[i]} - `{choice['choice_name']}`" for i, choice in enumerate(choices)]))
+            message = await ctx.send(_("__Choose the target :__\n")+'\n'.join([f"{reactions[i]} - `{choice['choice_name']}`" for i, choice in enumerate(choices)]))
             self.bot.loop.create_task(misc.add_reactions(message, reactions[:len(choices)]))
 
             try:
-                reaction, _ = await self.bot.wait_for('reaction_add', timeout=120, check=lambda react, usr: str(react.emoji) in reactions[:len(choices)] and usr.id == ctx.author.id and react.message.id == message.id)
+                reaction, __ = await self.bot.wait_for('reaction_add', timeout=120,
+                                                       check=lambda react, usr: str(react.emoji) in reactions[:len(choices)] and usr.id == ctx.author.id and react.message.id == message.id)
             except TimeoutError:
                 return await message.delete()
 
@@ -126,12 +136,7 @@ class Tag(commands.Cog):
         embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
 
         text = f'/tag {category} {query}'
-        url = discord.Embed.Empty
-        creator = await self.bot.fetch_user(tag.get('author')) if tag.get('author') else None
-
-        if creator:
-            text += f' • par {creator.name}#{creator.discriminator}'
-            url = creator.avatar_url
+        url = self.bot.avatar_url
         embed.set_footer(
             text=text,
             icon_url=url
@@ -140,7 +145,7 @@ class Tag(commands.Cog):
         if message: await message.edit(embed=embed, content="")
         else: message = await ctx.channel.send(embed=embed)
 
-        try: await ctx.message.delete()  # suppression de la commande
+        try: await ctx.message.delete()  # delete the trigger message (the command)
         except: pass
         try: await misc.delete_with_emote(ctx, message)
         except: pass
@@ -149,4 +154,3 @@ class Tag(commands.Cog):
 def setup(bot):
     bot.add_cog(Tag(bot))
     bot.logger.info("Extension [tag] loaded successfully.")
-
