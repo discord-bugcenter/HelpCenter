47ef5e590965ece7f1994cafac0e2b6afb597255
AiroPi <47398145+AiroPi@users.noreply.github.com>
2021-02-15 17:19:19 +0100

    feat/graphic event stats

--- cogs/event.py
+++ cogs/event.py
@@ -1,8 +1,12 @@
 import re
+import io
 import asyncio
+from functools import partial
 
 import discord
 from discord.ext import commands
+import matplotlib.pyplot as plt
+from matplotlib.ticker import StrMethodFormatter
 
 from .utils import custom_errors
 from .utils.i18n import use_current_gettext as _
@@ -129,12 +133,16 @@ class Event(commands.Cog):
 
         user_length = None
         list_of_length = []
+        datas = {}
         async with ctx.channel.typing():
             async for message in code_channel.history(limit=None):
                 if message.author.id != self.bot.user.id: continue
                 if str(ctx.author.id) == message.embeds[0].fields[0].value.split('|')[0]:
                     user_length = int(message.embeds[0].fields[2].value)
                 list_of_length.append(int(message.embeds[0].fields[2].value))
+                language = message.embeds[0].fields[1].value
+                datas.setdefault(language, 0)
+                datas[language] += 1
         list_of_length.sort()
 
         embed = discord.Embed(
@@ -145,8 +153,33 @@ class Event(commands.Cog):
         embed.add_field(name=_("Shortest participation (not tested) :"), value=str(min(list_of_length)), inline=False)
         if user_length:
             embed.add_field(name=_('Your position :'), value=str(list_of_length.index(user_length)+1))
+        embed.set_image(url="attachment://graph.png")
 
-        await ctx.channel.send(embed=embed)
+        fn = partial(self.create_graph_bars, datas)
+        final_buffer = await self.bot.loop.run_in_executor(None, fn)
+
+        file = discord.File(filename="graph.png", fp=final_buffer)
+
+        await ctx.channel.send(embed=embed, file=file)
+
+    @staticmethod
+    def create_graph_bars(datas):
+        fig, ax = plt.subplots()
+        langs = datas.keys()
+        values = datas.values()
+        ax.bar(langs, values,
+               color=misc.Color(10, 100, 255, 0.5).mpl,
+               edgecolor=misc.Color(10, 100, 255).mpl,
+               linewidth=5)
+
+        ax.yaxis.set_major_formatter(StrMethodFormatter('{x:,.0f}'))  # No decimal places
+        ax.set_yticks(range(1, max(values) + 1))
+        ax.set_title(_("Breakdown by languages used."))
+        buff = io.BytesIO()
+        fig.savefig(buff)
+        buff.seek(0)
+
+        return buff
 
 
 def setup(bot):
