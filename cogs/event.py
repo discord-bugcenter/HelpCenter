f91d264488ce051c50a3176eb76a0db9214459f2
AiroPi <47398145+AiroPi@users.noreply.github.com>
2021-03-06 00:27:42 +0100

    feat/event participate and checks
    The bot now check if the event is open, close, or end.
    It checks also languages.

--- cogs/event.py
+++ cogs/event.py
@@ -2,6 +2,7 @@ import re
 import io
 import asyncio
 from functools import partial
+from datetime import datetime
 
 import discord
 from discord.ext import commands
@@ -11,6 +12,62 @@ from matplotlib.ticker import StrMethodFormatter
 from .utils import custom_errors
 from .utils.i18n import use_current_gettext as _
 from .utils import misc
+
+RE_DESC_EVENT_DATE = re.compile(r'event-date : (\d{,2})/(\d{,2})/(\d{4})')
+RE_DESC_EVENT_STATE = re.compile(r'event-state : (\S+)')
+CODE_CHANNEL_ID = 810511403202248754
+
+LANGAGE_COMPREHENSION = {
+    'py': 'python3',
+    'python': 'python3',
+    'python3': 'python3',
+    'js': 'javascript',
+    'javascript': 'javascript',
+    'cpp': 'cpp',
+    'c++': 'cpp',
+    'bash': 'bash',
+    'c': 'c',
+    'csharp': 'csharp',
+    'go': 'go',
+    'haskell': 'haskell',
+    'java': 'java',
+    'kotlin': 'kotlin',
+    'node': 'javascript',
+    'perl': 'perl',
+    'php': 'php',
+    'ruby': 'ruby',
+    'rust': 'rust',
+    'swift': 'swift',
+    'typescript': 'javascript'
+}
+
+
+def event_not_closed():
+    async def inner(ctx):
+        code_channel = ctx.bot.get_channel(CODE_CHANNEL_ID)
+        state = RE_DESC_EVENT_STATE.search(code_channel.topic).group(1)
+
+        if state == 'closed':
+            await ctx.send(_('There is no event right now, sorry !'), delete_after=5)
+            return False
+
+        return True
+
+    return commands.check(inner)
+
+
+def event_not_ended():
+    async def inner(ctx):
+        code_channel = ctx.bot.get_channel(CODE_CHANNEL_ID)
+        state = RE_DESC_EVENT_STATE.search(code_channel.topic).group(1)
+
+        if state == 'ended':
+            await ctx.send(_('The event is ended, sorry !'), delete_after=5)
+            return False
+
+        return True
+
+    return commands.check(inner)
 
 
 class Event(commands.Cog):
@@ -43,8 +100,11 @@ class Event(commands.Cog):
         usage="/event participate {code}"
     )
     @commands.dm_only()
+    @event_not_ended()
+    @event_not_closed()
     async def participate(self, ctx, *, code):
         code_channel = self.bot.get_channel(self.code_channel_id)
+        day, month, year = RE_DESC_EVENT_DATE.search(code_channel.topic).groups()
 
         regex = re.search(r'(```)?(?:(\S*)\s)(\s*\S[\S\s]*)(?(1)```|)', code)
         if not regex:
@@ -52,16 +112,18 @@ class Event(commands.Cog):
         language, code = regex.groups()[1:]
         if len(code) > 1000:
             return await ctx.send(_("Looks like your code is too long! Try to remove the useless parts, the goal is to have a short and optimized code!"))
+        if language.lower() not in LANGAGE_COMPREHENSION.keys():
+            return await ctx.send(_('Your language seems not be valid for the event.'))
 
         old_participation = None
-        async for message in code_channel.history(limit=None):
+        async for message in code_channel.history(limit=None, after=datetime(int(year), int(month), int(day))):
             if message.author.id != self.bot.user.id: continue
             if str(ctx.author.id) == message.embeds[0].fields[0].value.split('|')[0]:
                 old_participation = message
                 break
 
         valid_message = await ctx.send(_('**This is your participation :**\n\n') +
-                                       _('`Language` -> `{0}`\n').format(language) +
+                                       _('`Language` -> `{0}`\n').format(LANGAGE_COMPREHENSION[language.lower()]) +
                                        _('`Length` -> `{0}`\n').format(len(code)) +
                                        f'```{language}\n{code}```\n' +
                                        _('Do you want ot post it ? ✅ ❌'))
@@ -77,8 +139,9 @@ class Event(commands.Cog):
                 color=misc.Color.grey_embed().discord
             )
             embed.add_field(name='User', value=f'{ctx.author.id}|{ctx.author.mention}', inline=False)
-            embed.add_field(name='Language', value=language, inline=True)
+            embed.add_field(name='Language', value=LANGAGE_COMPREHENSION[language.lower()], inline=True)
             embed.add_field(name='Length', value=str(len(code)), inline=True)
+            embed.add_field(name='Date', value=str(datetime.now().isoformat()), inline=False)
             embed.add_field(name='Code', value=f"```{language}\n{code}\n```", inline=False)
 
             if old_participation:
@@ -99,14 +162,17 @@ class Event(commands.Cog):
         description=_('Remove your participation from the contest'),
         usage="/event cancel"
     )
+    @event_not_ended()
+    @event_not_closed()
     async def cancel(self, ctx):
         if ctx.guild and ctx.channel.id not in self.bot.test_channels_id:  # Not in dm or in tests channels
             raise custom_errors.NotAuthorizedChannels(ctx.channel, self.bot.test_channels_id)
 
         code_channel = self.bot.get_channel(self.code_channel_id)
+        day, month, year = RE_DESC_EVENT_DATE.search(code_channel.topic).groups()
 
         old_participation = None
-        async for message in code_channel.history(limit=None):
+        async for message in code_channel.history(limit=None, after=datetime(int(year), int(month), int(day))):
             if message.author.id != self.bot.user.id: continue
             if str(ctx.author.id) == message.embeds[0].fields[0].value.split('|')[0]:
                 old_participation = message
@@ -125,34 +191,42 @@ class Event(commands.Cog):
         description=_("Get some stats about the current contest"),
         usage="/event stats"
     )
+    @event_not_closed()
     async def stats(self, ctx):
         if ctx.guild and ctx.channel.id not in self.bot.test_channels_id:  # Not in dm or in tests channels
             raise custom_errors.NotAuthorizedChannels(ctx.channel, self.bot.test_channels_id)
 
         code_channel = self.bot.get_channel(self.code_channel_id)
+        day, month, year = RE_DESC_EVENT_DATE.search(code_channel.topic).groups()
 
         user_length = None
         list_of_length = []
         datas = {}
         async with ctx.channel.typing():
-            async for message in code_channel.history(limit=None):
+            async for message in code_channel.history(limit=None, after=datetime(int(year), int(month), int(day))):
                 if message.author.id != self.bot.user.id: continue
-                if str(ctx.author.id) == message.embeds[0].fields[0].value.split('|')[0]:
-                    user_length = int(message.embeds[0].fields[2].value)
-                list_of_length.append(int(message.embeds[0].fields[2].value))
+                length = int(message.embeds[0].fields[2].value)
                 language = message.embeds[0].fields[1].value
-                datas.setdefault(language, 0)
-                datas[language] += 1
+
+                if str(ctx.author.id) == message.embeds[0].fields[0].value.split('|')[0]:
+                    user_length = length
+                    user_language = language
+
+                list_of_length.append(length)
+                datas.setdefault(language, [])
+                datas[language].append(length)
+
         list_of_length.sort()
+        [v.sort() for v in datas.values()]
 
         embed = discord.Embed(
             title=_('Some informations...'),
             color=misc.Color.grey_embed().discord
         )
         embed.add_field(name=_("Number of participations :"), value=str(len(list_of_length)), inline=False)
-        embed.add_field(name=_("Shortest participation (not tested) :"), value=str(min(list_of_length)), inline=False)
         if user_length:
-            embed.add_field(name=_('Your position :'), value=str(list_of_length.index(user_length)+1))
+            embed.add_field(name=_('Your global position :'), value=str(list_of_length.index(user_length)+1))
+            embed.add_field(name=_('Your position by language :'), value=str(datas[user_language].index(user_length)+1))
         embed.set_image(url="attachment://graph.png")
 
         fn = partial(self.create_graph_bars, datas, _("Breakdown by languages used."))
@@ -166,7 +240,7 @@ class Event(commands.Cog):
     def create_graph_bars(datas, title):  # title in arguments because translations doesn't work in a separated thread
         fig, ax = plt.subplots()
         langs = datas.keys()
-        values = datas.values()
+        values = [len(v) for v in datas.values()]
         ax.bar(langs, values,
                color=misc.Color(10, 100, 255, 0.5).mpl,
                edgecolor=misc.Color(10, 100, 255).mpl,
