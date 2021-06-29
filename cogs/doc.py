25927f145da2cd2fde42c922bd717aa5e9cd66b5
Lambdα#4866 <70655051+PetitPotiron@users.noreply.github.com>
2021-06-29 13:18:52 +0200

    Add files via upload

---
+++ cogs/doc.py
@@ -0,0 +1,34 @@
+from urllib import parse
+
+from discord.ext import commands
+
+from .utils import checkers
+from .utils.misc import delete_with_emote
+from .utils.i18n import use_current_gettext as _
+import re
+
+class Doc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(
+        name='doc',
+        usage='/doc {query}',
+        aliases=['docu', 'documentation'],
+        description=_(
+            'Shows a documentation for discord.js or discord.py :D')
+    )
+    @checkers.authorized_channels() # Using query (with *, arg) instead of array (*arg) to prevent argument missing.
+    async def doc(self, ctx, docname, *, query):
+        if re.findall(r'[p|Py|Y]', docname):
+            response = await ctx.send(_("This is the api reference for {}!\n").format(query) +
+                                      f"<https://discordpy.readthedocs.io/en/stable/api.html#{query}>")
+            await delete_with_emote(ctx, response)
+        elif re.findall(r'[j|Js|S]', docname):
+            response = await ctx.send(_("This is the api reference for {}!\n").format(query) +
+                                      f"<https://discord.js.org/#/docs/main/stable/class/{query}>")
+
+
+def setup(bot):
+    bot.add_cog(Doc(bot))
+    bot.logger.info("Extension [doc] loaded successfully.")
