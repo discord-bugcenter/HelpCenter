8a454767c6f44a166ab94f38d6b8f4073baac254
Pierre <pierrelixenbuhl@gmail.com>
2021-07-28 22:19:22 +0200

    feat/create a way to open personnal help threads !
    Just by clicking on a button, you will be able to ask your question in a separate thread channel and get better answers!

--- cogs/utils/checkers.py
+++ cogs/utils/checkers.py
@@ -1,8 +1,11 @@
+from typing import Union
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import check
 
 from ..utils import custom_errors
+from main import HelpCenterBot
 
 
 def authorized_channels_check(ctx: commands.Context) -> bool:
@@ -16,12 +19,23 @@ def authorized_channels():
     return check(authorized_channels_check)
 
 
+def is_high_staff_check(bot: HelpCenterBot, user: Union[discord.Member, discord.User]) -> tuple[bool, list[id]]:
+    bug_center: discord.Guild = bot.get_guild(bot.bug_center_id)
+
+    member: discord.Member = user
+    if isinstance(user, discord.User):
+        member = bug_center.get_member(user.id)
+
+    allowed_roles_ids: list[int] = [value for (key, value) in bot.staff_roles.items() if key in ('administrator', 'assistant', 'screening')]
+
+    return discord.utils.find(lambda r: r.id in allowed_roles_ids, member.roles) or member.guild_permissions.administrator, allowed_roles_ids
+
+
 def is_high_staff():
     async def inner(ctx: commands.Context):
-        member: discord.Member = auth if isinstance(auth := ctx.author, discord.Member) else ctx.bot.get_guild(ctx.bot.bug_center_id).get_member(ctx.author.id)
-        allowed_roles_ids = [value for key, value in ctx.bot.staff_roles.items() if key in ['administrator', 'assistant', 'screening', 'brillant']]
-        if discord.utils.find(lambda r: r.id in allowed_roles_ids, member.roles) or member.permissions_in(ctx.channel).administrator:
+        result, list_ids = is_high_staff_check(ctx.bot, ctx.author)
+        if result:
             return True
-        raise custom_errors.NotAuthorizedRoles(allowed_roles_ids)
+        raise custom_errors.NotAuthorizedRoles(list_ids)
 
     return check(inner)
