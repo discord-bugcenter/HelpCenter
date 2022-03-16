be02144620710386fbfd0c7f679b6741b2d60967
Pierre <pierrelixenbuhl@gmail.com>
2022-03-17 00:00:57 +0100

    Fix lines command and change i18n translation system.

--- cogs/utils/i18n.py
+++ cogs/utils/i18n.py
@@ -1,13 +1,15 @@
 #  i18n for discord bot, source here :
 #  https://github.com/XuaTheGrate/i18n/blob/master/i18n.py
 
-import contextvars
 import gettext
 import os.path
 from glob import glob
+from typing import Optional
+
+import discord
 
 BASE_DIR = "ressources/"  # change this if you store your files under `src/` or similar
-LOCALE_DEFAULT = 'en_US'
+LOCALE_DEFAULT = 'en-US'
 LOCALE_DIR = "locale"
 locales = frozenset(map(os.path.basename, filter(os.path.isdir, glob(os.path.join(BASE_DIR, LOCALE_DIR, '*')))))
 
@@ -19,22 +21,25 @@ gettext_translations = {
         localedir=os.path.join(BASE_DIR, LOCALE_DIR))
     for locale in locales}
 
-gettext_translations['en_US'] = gettext.NullTranslations()
-locales |= {'en_US'}
+gettext_translations['en-US'] = gettext.NullTranslations()
+locales |= {'en-US'}
 
 
-def use_current_gettext(*args, **kwargs):
+def get_translation(message, inter: Optional[discord.Interaction] = None):
+    if not inter:
+        locale = LOCALE_DEFAULT
+    else:
+        locale = inter.locale.value
+
     if not gettext_translations:
-        return gettext.gettext(*args, **kwargs)
+        return gettext.gettext(message)
 
-    locale = current_locale.get()
     return (
         gettext_translations.get(
             locale,
             gettext_translations[LOCALE_DEFAULT]
-        ).gettext(*args, **kwargs)
+        ).gettext(message)
     )
 
 
-current_locale = contextvars.ContextVar('i18n')
-current_locale.set(LOCALE_DEFAULT)
+_ = get_translation
