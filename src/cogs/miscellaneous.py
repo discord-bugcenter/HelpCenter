94f845fa8d53f94f57ec8896248b512a9699c67b
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-08-05 22:42:42 +0200

    refactor: miscellaneous
    Send files to gist are now done using context menu.

--- src/cogs/miscellaneous.py
+++ src/cogs/miscellaneous.py
@@ -1,131 +1,130 @@
+from __future__ import annotations
+
 import asyncio
 import os
 import re
+from functools import partial
+from typing import TYPE_CHECKING, Literal, cast
 
 import aiohttp
 import discord
-import filetype
+from discord import SelectOption, app_commands, ui
 from discord.ext import commands
+from typing_extensions import Self  # TODO: remove on 3.11 release
 
-from main import HelpCenterBot
-from utils.misc import add_reactions
+from utils.constants import BUG_CENTER_ID
+from utils.custom_errors import CustomError
 from utils.gist import create_new_gist, delete_gist
-from utils.i18n import _
-from utils.constants import BUG_CENTER_ID, AUTHORIZED_CHANNELS_IDS
+
+if TYPE_CHECKING:
+    from re import Pattern
+
+    from main import HelpCenterBot
 
 
-GIST_TOKEN = os.environ['GIST_TOKEN']
+GIST_TOKEN = os.environ["GIST_TOKEN"]
 
 
 class Miscellaneous(commands.Cog):
     def __init__(self, bot: HelpCenterBot) -> None:
         """Miscellaneous will check for files in messages and will convert is as gist, and will also check for discord tokens."""
-        self.bot = bot
-        self.re_token = re.compile(r"[\w\-=]{24}\.[\w\-=]{6}\.[\w\-=]{27}", re.ASCII)
+        self.bot: HelpCenterBot = bot
+        self.re_token: Pattern[str] = re.compile(r"[\w\-=]{24}\.[\w\-=]{6}\.[\w\-=]{27}", re.ASCII)
+
+        self.attachement_to_gist_ctx_menu = app_commands.ContextMenu(
+            name="Make a gist", callback=self.attachement_to_gist
+        )
+        self.bot.tree.add_command(self.attachement_to_gist_ctx_menu, guild=discord.Object(id=BUG_CENTER_ID))
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
-        if await self.token_revoke(message):
-            return
-
-        if message.channel.id not in AUTHORIZED_CHANNELS_IDS:
-            return
-
-        await self.attachement_to_gist(message)
+        """Look for discord token on message received."""
+        await self.token_revoke(message)
 
     @commands.Cog.listener()
     async def on_message_edit(self, old_message: discord.Message, new_message: discord.Message) -> None:
         """Look for discord token on message editing."""
-
         await self.token_revoke(new_message)
 
-    async def attachement_to_gist(self, message: discord.Message) -> None:
+    # context_menu command
+    async def attachement_to_gist(self, inter: discord.Interaction, message: discord.Message) -> None:
         if not message.attachments:
-            return
+            raise CustomError("Le message doit contenir un fichier.")
+
+        if len(message.attachments) > 1:
+
+            class SelectFileView(ui.View):
+                inter: discord.Interaction
+                select: ui.Select[SelectFileView]
+
+                @ui.select(
+                    options=[
+                        SelectOption(label=attachment.filename, value=str(i))
+                        for i, attachment in enumerate(message.attachments)
+                    ]
+                )
+                async def select_file(self, inter: discord.Interaction, select: ui.Select[SelectFileView]) -> None:
+                    self.inter = inter
+                    self.select = select
+                    self.stop()
+
+            view = SelectFileView()
+            await inter.response.send_message(
+                "Sélectionnez le fichier que vous souhaitez envoyer sur gist.", view=view, ephemeral=True
+            )
+            await view.wait()
+            inter = view.inter
+
+            attachment = message.attachments[int(view.select.values[0])]
         else:
             attachment = message.attachments[0]
 
         file = await message.attachments[0].read()
-        if filetype.guess(file) is not None:
-            return
 
         try:
-            file_content = file.decode('utf-8')
+            file_content = file.decode("utf-8")
         except UnicodeDecodeError:
             return
 
-        if await self.search_for_token(message, file_content):
-            return
-
-        await message.add_reaction('🔄')
-        try:
-            __, user = await self.bot.wait_for('reaction_add', check=lambda react, usr: not usr.bot and react.message.id == message.id and str(react.emoji) == '🔄', timeout=600)
-        except asyncio.TimeoutError:
-            return
-        finally:
-            await message.clear_reactions()
-
-        references = {
-            '<:javascript:664540815086845952>': 'js',
-            '<:python:664539154838978600>': 'py',
-            '<:html:706981296710352997>': 'html',
-            '<:php:664540814944370689>': 'php',
-            '<:java:664540814772273163>':  'java',
-            '<:go:665975979402985483>': 'go',
-            '<:lua:664539154788515902>': 'lua',
-            '<:ruby:664540815078588436>': 'rb',
-            '<:rust:664539155329581094>': 'rs',
-            '<:scala:665967129660751883>': 'scala',
-            '<:swift:664540815821111306>': 'swift'
-        }
-
-        response_message = None
-        if os.path.splitext(attachment.filename)[1] in tuple(f'.{ext}' for ext in references.values()):
+        if txt := (os.path.splitext(attachment.filename)[1] != ".txt"):
             file_name = attachment.filename
         else:
-            response_message = await message.reply((_("What's the programmation language ?\n", message.author) +
-                                                    _("Click on the correspondant reaction, or send a message with the extension (`.js`, `.py`...)\n\n", message.author) +
-                                                    f"{' '.join(references.keys())}"), mention_author=False)
 
-            task = self.bot.loop.create_task(add_reactions(response_message, references.keys()))
+            class SelectExtension(ui.Modal, title="Quel est l'extension du fichier ?"):
+                inter: discord.Interaction
 
-            done, pending = await asyncio.wait([
-                self.bot.wait_for('message', timeout=120, check=lambda msg: msg.author.id == user.id and msg.channel.id == response_message.channel.id and len(msg.content) < 7 and msg.content.startswith('.')),
-                self.bot.wait_for('reaction_add', timeout=120, check=lambda react, usr: usr.id == user.id and str(react.emoji) in references.keys())
-            ], return_when=asyncio.FIRST_COMPLETED)
+                extension: ui.TextInput[Self] = ui.TextInput(
+                    label="Extension", placeholder=".txt", min_length=1, max_length=10
+                )
 
-            try:
-                stuff = done.pop().result()
-                if isinstance(stuff, tuple):  # A reaction has been added
-                    file_name = f"code.{references.get(str(stuff[0].emoji))}"
-                else:
-                    file_name = f"code{stuff.content}"
-            except asyncio.TimeoutError:
-                return
-            finally:
-                task.cancel()
-                await response_message.clear_reactions()
-                for future in done:
-                    future.exception()
-                for future in pending:
-                    future.cancel()
-        async with message.channel.typing():
-            try:
-                json_response = await create_new_gist(GIST_TOKEN, file_name, file_content)
-                assert json_response.get('html_url')
-            except Exception:
-                await message.channel.send(_('An error occurred.', message.author), delete_after=5)
-                return
+                async def on_submit(self, interaction: discord.Interaction) -> None:
+                    self.inter = interaction
+                    self.stop()
 
-        if not response_message:
-            await message.reply(content=_("A gist has been created :\n", message.author) + f"<{json_response['html_url']}>", mention_author=False)
-        else:
-            await response_message.edit(content=_("A gist has been created :\n", message.author) + f"<{json_response['html_url']}>")
+            modal = SelectExtension()
+            await inter.response.send_modal(modal)
+            await modal.wait()
+            inter = modal.inter
 
-    async def token_revoke(self, message):
-        tokens_places = [
-            message.clean_content
-        ]
+            ext = cast(str, modal.extension.value)
+            file_name = attachment.filename[: -4 * txt] + ext[ext.startswith(".") :]
+
+        await inter.response.defer(ephemeral=True, thinking=True)
+        try:
+            json_response = await create_new_gist(GIST_TOKEN, file_name, file_content)
+            json_response["html_url"]
+        except Exception:
+            raise CustomError("Impossible de créer le gist.")
+
+        strategy = (
+            inter.edit_original_message
+            if inter.response.is_done()
+            else partial(inter.response.send_message, ephemeral=True)
+        )
+        await strategy(content="Un gist a été créé :\n" + f"<{json_response['html_url']}>")
+
+    async def token_revoke(self, message: discord.Message) -> Literal[True] | None:
+        tokens_places = [message.clean_content]
 
         for embed in message.embeds:
             tokens_places.append(str(embed.author))
@@ -145,55 +144,59 @@ class Miscellaneous(commands.Cog):
     async def search_for_token(self, message: discord.Message, text: str) -> bool:
         if not (match := self.re_token.search(text)):
             return False
-        headers = {
-            "Authorization": f"Bot {match.group(0)}"
-        }
-        url = "https://discord.com/api/v9/users/@me"
+        headers = {"Authorization": f"Bot {match.group(0)}"}
+        url = "https://discord.com/api/v10/users/@me"
         async with aiohttp.ClientSession(headers=headers) as session:
             async with session.get(url=url) as response:
                 if response.status == 200:
                     await message.delete()
                     response_dict = await response.json()
 
-                    message_content = _("**{message.author.mention} you just sent a valid bot token.**\n", message.author).format(message=message)
-                    message_content += _("This one will be revoked, but be careful and check that it has been successfully reset on the "
-                                         "**dev portal** (https://discord.com/developers/applications/{}).\n", message.author).format(response_dict['id'])
+                    message_content = f"**{message.author.mention} Vous venez d'envoyer un token de bot valide.**\n"
+                    message_content += (
+                        f"Celui là a été réinitialisé automatiquement, mais réinitialisez-le aussi vous même sur le "
+                        f"**portail dev** (https://discord.com/developers/applications/{response_dict['id']}).\n"
+                    )
 
                     await message.channel.send(message_content, allowed_mentions=discord.AllowedMentions(users=True))
 
-                    gist = await create_new_gist(GIST_TOKEN, 'token revoke', match.group(0))
+                    gist = await create_new_gist(GIST_TOKEN, "token revoke", match.group(0))
                     await asyncio.sleep(30)
-                    await delete_gist(GIST_TOKEN, gist['id'])
+                    await delete_gist(GIST_TOKEN, gist["id"])
                     return True
 
         # Check if it is eventually a user token.
-        url = "https://discord.com/api/v9/users/@me"
-        headers = {
-            "Authorization": match.group(0)
-        }
+        url = "https://discord.com/api/v10/users/@me"
+        headers = {"Authorization": match.group(0)}
         async with aiohttp.ClientSession(headers=headers) as session:
             async with session.get(url=url) as response:
                 if response.status == 200:
                     await message.delete()
-                    text = _(f"{message.author.mention} you just sent a valid **user token**.\n"
-                             "**What is it? ** This is a kind of password that allows access to a contentious account without a username, password or IP address verification.\n"
-                             "**Change your password as a precaution**.\n"
-                             "We also recommend that you enable**two-factor authentication, or 2FA.** (settings)\n", message.author)
+                    text = (
+                        f"{message.author.mention} vous venez d'envoyer un **token utilisateur**.\n"
+                        "**Qu'est-ce que c'est ? ** C'est une sorte de mot de passe qui autorise à contrôler votre compte.\n"
+                        "**Changez immédiatement votre mot de passe par précaution**.\n"
+                        "Nous vous conseillons aussi d'activer **l'authentification à double facteur (2FA)** si c'est n'est pas encore fait.\n"
+                    )
                     await message.channel.send(text, allowed_mentions=discord.AllowedMentions(users=True))
                     return True
 
         return False
 
     @commands.Cog.listener()
-    async def on_member_update(self, old_member: discord.Member, new_member: discord.Member):
+    async def on_member_update(self, old_member: discord.Member, new_member: discord.Member) -> None:
         if new_member.guild.id != BUG_CENTER_ID:
             return
         if len(new_member.roles) == 1:
             return
 
-        all_separator_roles: list[discord.Role] = [role for role in new_member.guild.roles[1:] if role.name == '━━━━━━━━━━━━━━━ㅤ']
+        all_separator_roles: list[discord.Role] = [
+            role for role in new_member.guild.roles[1:] if role.name == "━━━━━━━━━━━━━━━ㅤ"
+        ]
 
-        separator_roles: list[discord.Role] = [role for role in all_separator_roles if role.position > new_member.roles[1].position]
+        separator_roles: list[discord.Role] = [
+            role for role in all_separator_roles if role.position > new_member.roles[1].position
+        ]
         needed_separators: list[discord.Role] = []
 
         member_roles: list[discord.Role] = [role for role in new_member.roles[1:] if role not in separator_roles]
