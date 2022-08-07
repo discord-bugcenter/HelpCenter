232237e283dc858428f7a1bd9b7a4f7f3451b848
AiroPi <47398145+AiroPi@users.noreply.github.com>
2022-08-07 23:24:43 +0200

    refactor: cleanup code (again)
    - better error handler
    - fix minor bugs
    - remove some useless code/functions/comments

--- src/main.py
+++ src/main.py
@@ -6,15 +6,12 @@ import typing
 import discord
 from discord.ext import commands
 
-from utils import custom_errors
-from utils.constants import BUG_CENTER_ID  # , LANGUAGE_ROLES
+from utils.constants import BUG_CENTER_ID
 from utils.custom_command_tree import CustomCommandTree
 from utils.logger import INFO, create_logger
 
 if typing.TYPE_CHECKING:
     from logging import Logger
-
-    from discord.ext.commands import Context
 
 
 LOG_LEVEL = int(tmp) if (tmp := os.getenv("LOG_LEVEL")) and tmp.isdigit() else INFO
@@ -44,9 +41,6 @@ class HelpCenterBot(commands.Bot):
             "cogs.doc",
         ]
 
-        # self.before_invoke(self.set_command_language)
-        self.add_check(self.is_on_bug_center)
-
     async def setup_hook(self) -> None:
         for ext in self.initial_extensions:
             try:
@@ -64,11 +58,6 @@ class HelpCenterBot(commands.Bot):
 
         logger.info(f"Logged in as : {bot_user.name}")
         logger.info(f"ID : {bot_user.id}")
-
-    def is_on_bug_center(self, ctx: commands.Context[HelpCenterBot]) -> bool:
-        if ctx.guild and ctx.guild.id != BUG_CENTER_ID:
-            raise custom_errors.NotInBugCenter()
-        return True
 
 
 if __name__ == "__main__":
--- src/utils/custom_command_tree.py
+++ src/utils/custom_command_tree.py
@@ -1,96 +1,39 @@
-from typing import TYPE_CHECKING, Any, Optional
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 import discord
 
 # from discord.ext import commands
-from discord.app_commands import (
-    AppCommandError,
-    CheckFailure,
-    Command,
-    CommandInvokeError,
-    CommandNotFound,
-    CommandTree,
-    ContextMenu,
-)
+from discord.app_commands import AppCommandError, CommandNotFound, CommandTree
 
-from utils import custom_errors as custom
+from utils import ExtendedColor
+from utils.custom_errors import CustomError
 
 if TYPE_CHECKING:
-    from main import HelpCenterBot  # , Context
+    from main import HelpCenterBot
 
 
-class CustomCommandTree(CommandTree[HelpCenterBot]):
-    def __init__(self, bot: HelpCenterBot) -> None:
-        """Handle error for the bot commands."""
-        self.bot: HelpCenterBot = bot
-        super().__init__(bot)
-
+class CustomCommandTree(CommandTree["HelpCenterBot"]):
     @staticmethod
-    async def send_error(inter: discord.Interaction, error_message: str):
+    async def send_error(inter: discord.Interaction, error_message: str) -> None:
         """A function to send an error message."""
-        await inter.response.send_message(error_message)
-        # embed = discord.Embed(
-        #     title="<:error:797539791545565184> Erreur",
-        #     url="https://discord.gg/Drbgufc",
-        #     description=error_message,
-        #     timestamp=ctx.message.created_at,
-        #     color=Color.black().discord
-        # )
-        # embed.set_author(
-        #     name=f'{ctx.author.name}#{ctx.author.discriminator}',
-        #     icon_url=ctx.author.display_avatar.url
-        # )
-        # embed.set_footer(
-        #     text=_(f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} open-source project").format(**locals()),
-        #     icon_url=ctx.bot.user.display_avatar.url
-        # )
+        embed = discord.Embed(colour=discord.Color.brand_red())
+        embed.set_author(
+            name=error_message,
+            icon_url="https://cdn.discordapp.com/attachments/584397334608084992/1005925420639735870/discord_error_icon.png",
+        )
+        await inter.response.send_message(embed=embed, ephemeral=True)
 
-        # return await ctx.send(embed=embed, delete_after=10)
-
-    async def on_error(
-        self, interaction: discord.Interaction, command: ContextMenu | Command[Any, (...), Any], error: AppCommandError
-    ) -> Optional[discord.Message]:
+    async def on_error(self, interaction: discord.Interaction, error: AppCommandError) -> None:
         """Function called when a command raise an error."""
 
-        if isinstance(error, CommandNotFound):
-            return
+        match error:
+            case CommandNotFound():
+                return
+            case CustomError():
+                return await self.send_error(interaction, str(error))
+            case _:
+                await self.send_error(interaction, "Une erreur inconnue est survenue.")
 
-        # Custom errors :
-
-        if isinstance(error, custom.NotAuthorizedChannels):
-            formatted_text = (
-                _(
-                    "You can't execute this command in <#{inter.channel.id}>. Try in one of these channels :\n\n", inter
-                ).format(**locals())
-                + f"<#{'>, <#'.join(str(channel_id) for channel_id in error.list_channels_id)}>"
-            )
-            return await self.send_error(inter, formatted_text)
-        if isinstance(error, custom.NotAuthorizedRoles):
-            formatted_text = (
-                _("You can't execute this command, you need one of these roles :\n\n", inter).format(**locals())
-                + f"<@&{'>, <@&'.join(str(role_id) for role_id in error.list_roles_id)}>"
-            )
-            return await self.send_error(inter, formatted_text)
-        # if isinstance(error, custom.COCLinkNotValid):
-        #     return await self.send_error(inter, _("You send an invalid link/code, or the game cannot be joined anymore, or the game doesn't exist !"))
-        # if isinstance(error, custom.AlreadyProcessingCOC):
-        #     return await self.send_error(inter, _("This clash is already published !"))
-
-        # discord errors
-
-        # if isinstance(error, PrivateMessageOnly):
-        #     return await self.send_error(ctx, _('This command must be executed in Private Messages'))
-        if isinstance(error, CheckFailure):
-            return
-        if isinstance(error, CommandInvokeError):
-            if isinstance(error.__cause__, IndexError):
-                return await self.send_error(inter, _("That query didn't provide enough results.", inter))
-        if isinstance(error, AppCommandError):
-            return await self.send_error(inter, str(error))
-
-        # # Other errors
-
-        # if isinstance(error, NoPendingCOC):
-        #     return await self.send_error(ctx, _('There is no public coc started at the moment. Try again in a few seconds or go to https://www.codingame.com/multiplayer/clashofcode and click "Join a clash".'))
-
-        self.bot.logger.error(error)  # if the error is not handled
+        self.client.logger.error(error)
