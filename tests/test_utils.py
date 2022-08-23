30acf3a108ac1e4028dbd2aed4de02d646c9a69a
AiroPi <47398145+AiroPi@users.noreply.github.com>
2022-08-24 01:24:52 +0200

    feat: improve helping system
    - separate "create" request and list of requests
    - display author of thread
    - use raw events to handle more events than before

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,10 @@ from src.utils import ExtendedColor, constants
 
 
 def test_constants():
-    assert constants.ASK_CHANNEL_ID == 870023524985761822
-    assert constants.ASK_MESSAGE_ID == 870032630119276645
+    assert constants.NEW_REQUEST_CHANNEL_ID == 870023524985761822
+    assert constants.REQUEST_MESSAGE_ID == 870032630119276645
     assert constants.BUG_CENTER_ID == 595218682670481418
+    assert constants.REQUESTS_CHANNEL_ID == 1011603777138208858
 
 
 def test_extended_color():
